dcaebd6ad695dca0dcfce23411e59210232ceb33
corosica <corosica1@gmail.com>
2024-05-21 16:49:09 +0900

    QNA 게시판 제작

--- final_pjt_back/articles/models.py
+++ final_pjt_back/articles/models.py
@@ -15,4 +15,17 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+
+class QNAarticle(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class QNAcomment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
