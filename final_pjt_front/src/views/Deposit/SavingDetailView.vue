4be60f75b6d8dabf89b7a0dee10560b2ba21477c
yoojin22 <universe_adhoc@naver.com>
2024-05-22 17:54:29 +0900

    예적금 개월수  선택하기 추가

--- final_pjt_front/src/views/Deposit/SavingDetailView.vue
+++ final_pjt_front/src/views/Deposit/SavingDetailView.vue
@@ -1,100 +1,133 @@
 <template>
     <div class="container">
-        <h2><strong>정기 적금 상세</strong></h2>
-        <div class="info-header">
-            <button class="join-button" @click="join" v-if="check">탈퇴하기</button>
-            <button class="join-button" @click="join" v-else>가입하기</button>
+      <h2><strong>정기 적금 상세</strong></h2>
+      <br>
+      <div class="info-header">
+        <h4>{{ savingdata.fin_prdt_nm }}</h4>
+        <div v-if="check">
+          <button class="join-button" @click="join">해지하기</button>
         </div>
-        <div class="info">
-            <h4>{{ depositdata.fin_prdt_nm }}</h4>
-            <hr>
-            <p>공시 제출일: {{ depositdata.dcls_strt_day }}</p>
-            <p>은행: {{ depositdata.kor_co_nm }}</p>
-            <p v-if="depositdata.max_limit >= 100000000">한도: {{ depositdata.max_limit / 100000000 }}억원</p>
-            <p v-else-if="depositdata.max_limit > 10000">한도: {{ depositdata.max_limit / 10000 }}만원</p>
-            <p v-else>한도: 없음</p>
-            <p>가입 방법: {{ depositdata.join_way }}</p>
+        <div v-else>
+          <div class="join-section">
+            <select class="interest-options" v-model="selectedOption">
+              <option v-for="option in savingdata.saving_options" :key="option.id" :value="option">
+                {{ option.save_trm }}개월 - {{ option.rsrv_type_nm}}
+              </option>
+            </select>
+            <button class="join-button" @click="join">가입하기</button>
+          </div>
         </div>
-        <div class="conditions">
-            <p>우대 조건</p>
-            <p>{{ depositdata.spcl_cnd }}</p>
-            <p>{{ depositdata.etc_note }}</p>
-        </div>
-        <button class="back-button" @click="goBack">목록</button>
+      </div>
+      <div class="info">
+        <hr>
+        <p>공시 제출일: {{ savingdata.dcls_strt_day }}</p>
+        <p>은행: {{ savingdata.kor_co_nm }}</p>
+        <p v-if="savingdata.max_limit >= 100000000">한도: {{ savingdata.max_limit / 100000000 }}억원</p>
+        <p v-else-if="savingdata.max_limit > 10000">한도: {{ savingdata.max_limit / 10000 }}만원</p>
+        <p v-else>한도: 없음</p>
+        <p>가입 방법: {{ savingdata.join_way }}</p>
+        <p>기본 금리: {{ selectedOption?.intr_rate }}%</p>
+        <p>우대 금리: {{ selectedOption?.intr_rate2 }}%</p>
+      </div>
+      <div class="conditions">
+        <h4>우대 조건</h4>
+        <p>{{ savingdata.spcl_cnd }}</p>
+        <p>{{ savingdata.etc_note }}</p>
+      </div>
+      <button class="back-button" @click="goBack">목록</button>
     </div>
-</template>
-
-
-
-
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import {useCounterStore} from '@/stores/counter';
-import { useRouter,useRoute } from 'vue-router';
-const depositdata = ref([]);
-const counterStore = useCounterStore();
-const router = useRouter();
-const route  = useRoute();
-const how = ref('')
-const buyUser = ref([]);
-const check = ref(false);
-onMounted(async () => {
-    try{
-        await counterStore.getSavingDetail(route.params.id);
-        depositdata.value = counterStore.SavingDetails
-        how.value = depositdata.value.saving_options[0].rsrv_type_nm
-        buyUser.value = depositdata.value.buy_user;
-        check.value = (buyUser.value).includes(parseInt(sessionStorage.user_id));
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { useCounterStore } from '@/stores/counter';
+  import { useRouter, useRoute } from 'vue-router';
+  
+  const savingdata = ref([]);
+  const selectedOption = ref(null);
+  const counterStore = useCounterStore();
+  const router = useRouter();
+  const route = useRoute();
+  const buyUser = ref([]);
+  const check = ref(false);
+  
+  onMounted(async () => {
+    try {
+      await counterStore.getSavingDetail(route.params.id);
+      savingdata.value = counterStore.SavingDetails;
+      buyUser.value = savingdata.value.buy_user;
+      check.value = buyUser.value.includes(parseInt(sessionStorage.user_id));
+      if (savingdata.value.saving_options.length > 0) {
+        selectedOption.value = savingdata.value.saving_options[0];
+      }
     } catch (e) {
-        console.log(e)
+      console.log(e);
     }
-})
-
-
-const join = () => {
-    counterStore.buyProduct('saving',depositdata.value.id,depositdata.value.saving_options[0].id)
-    router.go(0)
-}
-
-const goBack = () => {
-    router.push({name:'SavingList'});
-};
-</script>
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&display=swap');
-
-* {
-  font-family: 'Noto Sans KR', sans-serif; /* 전역 폰트 적용 */
-}
-
-.container {
+  });
+  
+  const goBack = () => {
+    router.push({ name: 'SavingList' });
+  };
+  
+  const join = () => {
+    if (selectedOption.value) {
+      counterStore.buyProduct('saving', savingdata.value.id, selectedOption.value.id);
+      router.go(0);
+    } else {
+      alert('금리 옵션을 선택해주세요.');
+    }
+  };
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&display=swap');
+  
+  * {
+    font-family: 'Noto Sans KR', sans-serif; /* 전역 폰트 적용 */
+  }
+  
+  .container {
     max-width: 600px;
     margin: 30px auto;
     padding: 20px;
     background-color: #f7f7f7;
     border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
     position: relative;
-}
-
-h2 {
+  }
+  
+  h2 {
     color: #1abc9c;
     text-align: center;
-    margin-bottom: 10px;
-}
-
-.info-header {
-    position: relative;
-    height: 40px; /* Adjust the height as needed */
-}
-
-.join-button {
-    position: absolute;
-    right: 0;
-    top: 0;
+    margin-bottom: 20px; /* Add more space below the heading */
+  }
+  
+  .info-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px; /* Add more space below the info header */
+  }
+  
+  .info-header h4 {
+    margin: 0;
+  }
+  
+  .join-section {
+    display: flex;
+    align-items: center;
+  }
+  
+  .interest-options {
+    margin-right: 20px; /* Increase margin for more space */
+    padding: 8px;
+    font-size: 14px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  
+  .join-button {
     padding: 8px 16px;
     background-color: #1abc9c;
     color: #fff;
@@ -103,43 +136,39 @@ h2 {
     cursor: pointer;
     font-size: 16px;
     font-weight: bold;
-}
-
-.join-button:hover {
+  }
+  
+  .join-button:hover {
     background-color: #16a085;
-}
-
-.info p {
+  }
+  
+  .info p {
     margin: 10px 0;
     font-size: 16px;
     color: #333;
-}
-
-.info p strong {
-    color: #000;
-}
-
-.conditions {
+  }
+  
+  .conditions {
     background-color: #fff;
     border-left: 4px solid #1abc9c;
     padding: 15px;
     margin-top: 20px;
     border-radius: 8px;
-}
-
-.conditions p {
+  }
+  
+  .conditions p {
     margin: 5px 0;
     font-size: 14px;
     color: #333;
-}
-
-.conditions p:first-child {
+  }
+  
+  .conditions p:first-child {
     font-size: 16px;
     font-weight: bold;
     color: #1abc9c;
-}
-
-.back-button {
+  }
+  
+  .back-button {
     margin-top: 20px;
     padding: 10px 20px;
     background-color: #dfdfdf;
@@ -153,9 +182,10 @@ h2 {
     width: 100px;
     margin-left: auto;
     margin-right: auto;
-}
-
-.back-button:hover {
+  }
+  
+  .back-button:hover {
     background-color: #bbb;
-}
-</style>
+  }
+  </style>
+  
